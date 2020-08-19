3e7eea04ea45f66f8786358cd429bac3f6d7a074
Curtis Vanzandt <curtis.vanzandt@carecorenational.com>
2020-08-19 13:46:50 -0500

    Learning about useState hook for functional components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
+export const App = props => {
+    const [ peopleState, setPeopleState ] = useState({
         people: [
-            { name: "Curtis", age: 26 },
-            { name: "Hannah", age: 28 },
-            { name: "Blitz", age: 4 }
+            { name: 'Curtis', age: 26 },
+            { name: 'Hannah', age: 28 },
+            { name: 'Blitz', age: 4 },
         ],
         otherState: "some other value"
-    }
+    });
 
-    switchNameHandler = () => {
-        this.setState({
+    console.log(peopleState);
+
+    const switchNameHandler = () => {
+        setPeopleState({
             people: [
                 { name: "Curtis V", age: 26 },
                 { name: "Hannah V", age: 28 },
                 { name: "Blitz V", age: 4 }
-            ]})
-    }
+            ]
+        });
+    };
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hi, I am a React App</h1>
-                <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
-                <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Crocheting</Person>
-                <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <h1>Hi, I am a React App</h1>
+            <p>This is really working!</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={peopleState.people[0].name} age={peopleState.people[0].age}/>
+            <Person name={peopleState.people[1].name} age={peopleState.people[1].age}>My Hobbies: Crocheting</Person>
+            <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
+        </div>
+    );
 }
 
+
+
 export default App;
+
+// state = {
+//     people: [
+//         { name: "Curtis", age: 26 },
+//         { name: "Hannah", age: 28 },
+//         { name: "Blitz", age: 4 }
+//     ],
+//     otherState: "some other value"
+// }
+//
+
+
